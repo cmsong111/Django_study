ba63a72a7f7345e832dae45aa40effd32cc29aac
Kimnamju <cmsong111@naver.com>
2021-11-30 20:41:13 +0900

    UPDate

--- Book/mysite/polls/admin.py
+++ Book/mysite/polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-
 from .models import Question,Choice
 
 admin.site.register(Question)
 admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    filedsets = [
+        ('Question Statement', {'fields': ['question_text']}),
+        ('Date Infromation', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
