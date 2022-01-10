b4c46b6d644bfe2261f9ce01094cfebf69b60272
Namju Kim <cmsong111@naver.com>
2022-01-10 20:05:19 +0900

    update base,home html

--- NjProject/NjProject/views.py
+++ NjProject/NjProject/views.py
@@ -6,4 +6,7 @@ from django.apps import apps
 class HomeView(TemplateView):
 
     template_name = 'home.html'
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_list'] = ['todolist']
+        return context
